564c6355cdbc48462f5de3ca1a9eb969749c8712
Romy Tsoupidi <tsoupidi@kth.se>
2023-01-16 12:15:46 +0100

    Fix for new stack.

---
+++ src/solvers/multi_backend/portfolio/exec/portfolio/Main.hs
@@ -0,0 +1,16 @@
+{-|
+Copyright   :  Copyright (c) 2023
+License     :  BSD3 (see the LICENSE file)
+Maintainer  :  tsoupidi@kth.se
+-}
+{-
+Main authors:
+  Rodothea Myrsini Tsoupidi <tsoupidi@kth.se>
+
+This file is part of Unison, see http://unison-code.github.io
+-}
+module Main (main) where
+
+import Portfolio.Portfolio
+
+main = portmain 
